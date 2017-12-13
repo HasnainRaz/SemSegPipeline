73b7a9e7d5589ffe5e094a5169064f7f3b573c96
hasnain <hasnain3257@gmail.com>
2017-12-13 14:06:21 +0100

    Changed to use tf.cond for control flow, works as intended now

--- utility.py
+++ utility.py
@@ -3,76 +3,149 @@ from tensorflow.contrib.data import Dataset, Iterator
 import random
 
 
-def _resize_image(image, mask):
-    image = tf.image.resize_bicubic(image, [480, 640], True)
-    mask = tf.image.resize_bicubic(mask, [480, 640], True)
-    return image, mask
+def augmentation_chooser():
+    '''Returns a list of length 6, for use as the params placeholder for
+    the augmented_dataset function, just feed the list returned by this
+    function into your params place holder while training.'''
+    aug_to_apply = [not bool(random.randint(0, 7)),
+                    not bool(random.randint(0, 3)),
+                    not bool(random.randint(0, 3)),
+                    not bool(random.randint(0, 3)),
+                    not bool(random.randint(0, 1)),
+                    not bool(random.randint(0, 1))]
+
+    return aug_to_apply
 
 
-def _corrupt_contrast(image, mask):
-    image = tf.image.random_contrast(image, 0, 5)
-    return image, mask
+def augment_dataset(images, masks, params, crop_size, image_size, batch_size):
+    '''
+    Returns Augmented images with either: random_crop, random_brightness,
+    random_contrast, random_saturation, random_flip vertically, random_flip
+    horizontally, defined by the boolean tensor params
+    Inputs:
+        images: Batch of images, shape = [batch, width, height, channels]
+        masks: Batch of mask images, shape = [batch, width, height, channels]
+        params: Placeholder tensor to define which random perturbations to apply
+                out of the 6 mentioned above, shape = [6]
+    Returns:
+        images: Augmented images with any combination of augmentations applied
+        masks: Segmentation masks flipped/cropped in accord with their image
+    '''
 
-
-def _corrupt_saturation(image, mask):
-    image = tf.image.random_saturation(image, 0, 5)
-    return image, mask
-
-
-def _corrupt_brightness(image, mask):
-    image = tf.image.random_brightness(image, 5)
-    return image, mask
-
-
-def _random_crop(image, mask):
+    # Get seed to sync all random functions
     seed = random.random()
-    image = tf.random_crop(image, [240, 320, 3], seed=seed)
-    mask = tf.random_crop(mask, [240, 320, 1], seed=seed)
-    return image, mask
+    # Apply same cropping on image and mask with the help of seed,
+    # if true in params placeholder tensor
+    images = tf.cond(params[0], lambda: tf.random_crop(
+        images, [batch_size, int(crop_size[0] * 0.85), int(crop_size[1] * 0.85), 3], seed=seed), lambda: tf.identity(images))
+    masks = tf.cond(params[0], lambda: tf.random_crop(
+        masks, [batch_size, int(crop_size[0] * 0.85), int(crop_size[1] * 0.85), 1], seed=seed), lambda: tf.identity(masks))
 
+    # Apply random brightness changes if True in params placeholder tensor
+    images = tf.cond(params[1], lambda: tf.image.random_brightness(
+        images, 0.01), lambda: tf.identity(images))
 
-def _flip_image_horizontally(image, mask):
-    seed = random.random()
-    image = tf.image.random_flip_left_right(image, seed=seed)
-    mask = tf.image.random_flip_left_right(mask, seed=seed)
+    # Apply random contrast changes if True in params
+    images = tf.cond(params[2], lambda: tf.image.random_contrast(
+        images, 0.2, 1.8), lambda: tf.identity(images))
 
-    return image, mask
+    # Saturation doesn't work on image batches, only single images,
+    # Use map_fn to apply on batch
+    def sat(x): return tf.image.random_saturation(x, 0.2, 1.8)
+    images = tf.cond(params[3], lambda: tf.map_fn(
+        sat, images), lambda: tf.identity(images))
 
+    # Define tf.identity equivalent which returns two inputs unchanged
+    def unchanged(images, masks):
+        return images, masks
 
-def _flip_image_vertically(image, mask):
-    seed = random.random()
-    image = tf.image.random_flip_up_down(image, seed=seed)
-    mask = tf.image.random_flip_up_down(mask, seed=seed)
+    # Apply random horizontal flip if active in params,
+    # tf.image.random_flip_left_right doesnt work on image batches
+    def flip_l_r(images, masks):
+        image, mask = [], []
+        for i in range(batch_size):
+            seed = random.random()
+            image.append(tf.image.random_flip_left_right(
+                images[i, :, :, :], seed=seed))
+            mask.append(tf.image.random_flip_left_right(
+                masks[i, :, :, :], seed=seed))
+        images = tf.stack(image)
+        masks = tf.stack(mask)
+        return images, masks
 
-    return image, mask
+    # Apply horizontal flip
+    images, masks = tf.cond(params[4], lambda: flip_l_r(
+        images, masks), lambda: unchanged(images, masks))
+
+    # Apply random vertical flip if active in params,
+    # tf.image.random_flip_up_down doesnt work on image batches
+    def flip_u_d(images, masks):
+        image, mask = [], []
+        for i in range(batch_size):
+            seed = random.random()
+            image.append(tf.image.random_flip_up_down(
+                images[i, :, :, :], seed=seed))
+            mask.append(tf.image.random_flip_up_down(
+                masks[i, :, :, :], seed=seed))
+        images = tf.stack(image)
+        masks = tf.stack(mask)
+        return images, masks
+
+    # Apply the vertical flip
+    images, masks = tf.cond(params[5], lambda: flip_l_r(
+        images, masks), lambda: unchanged(images, masks))
+
+    # Finally resize the images to given width and height
+    images = tf.image.resize_bicubic(images, image_size)
+    masks = tf.image.resize_bicubic(masks, image_size)
+
+    return images, masks
 
 
 def _normalize_data(image, mask):
+    '''Returns image and mask divided by 255 (Vals in range 0.0 - 1.0)'''
     image = tf.cast(image, tf.float32)
     image = image / 255.0
 
     mask = tf.cast(mask, tf.float32)
     mask = mask / 255.0
+    mask = tf.cast(mask, tf.int32)
 
     return image, mask
 
 
 def _parse_data(image_paths, mask_paths):
+    '''Reads image and mask files'''
     image_content = tf.read_file(image_paths)
     mask_content = tf.read_file(mask_paths)
 
-    images = tf.image.decode_png(image_content, channels=3)
-    masks = tf.image.decode_png(mask_content, channels=1)
+    images = tf.image.decode_png(image_content, channels=0)
+    masks = tf.image.decode_png(mask_content, channels=0)
 
     return images, masks
 
 
-def data_batch(image_paths, mask_paths, params, batch_size=4, num_threads=2):
+def data_batch(image_paths, mask_paths, batch_size=4, num_threads=2):
+    '''Reads data, normalizes it, shuffles it, then batches it, returns a
+       the next element in dataset op and the dataset initializer op.
+       Inputs:
+        image_paths: A list of paths to individual images
+        mask_paths: A list of paths to individual mask images
+        batch_size: Number of images/masks in each batch returned
+        num_threads: Number of parallel calls to make
+       Returns:
+        next_element: A tensor with shape [2], where next_element[0]
+                      is image batch, next_element[1] is the corresponding
+                      mask batch
+        init_op: Data initializer op, needs to be executed in a session
+                 for the data queue to be filled up and the next_element op
+                 to yield batches'''
+
     # Convert lists of paths to tensors for tensorflow
     images_name_tensor = tf.constant(image_paths)
     mask_name_tensor = tf.constant(mask_paths)
 
-    # Create dataset out of the 3 files:
+    # Create dataset out of the 2 files:
     data = Dataset.from_tensor_slices(
         (images_name_tensor, mask_name_tensor))
 
@@ -80,42 +153,15 @@ def data_batch(image_paths, mask_paths, params, batch_size=4, num_threads=2):
     data = data.map(
         _parse_data, num_threads=num_threads, output_buffer_size=6 * batch_size)
 
-    # Concat. band 1, band 2 and composite images along the depth
+    # Normalize to be in range 0-1
     data = data.map(
-        _normalize_data, num_threads=num_threads, output_buffer_size=6 * batch_size)
-
-    if params['crop'] and not random.randint(0, 1):
-        data = data.map(_random_crop, num_threads=num_threads,
-                        output_buffer_size=6 * batch_size)
-
-    if params['brightness'] and not random.randint(0, 1):
-        data = data.map(_corrupt_brightness, num_threads=num_threads,
-                        output_buffer_size=6 * batch_size)
-
-    if params['contrast'] and not random.randint(0, 1):
-        data = data.map(_corrupt_contrast, num_threads=num_threads,
-                        output_buffer_size=6 * batch_size)
-
-    if params['saturation'] and not random.randint(0, 1):
-        data = data.map(_corrupt_saturation, num_threads=num_threads,
-                        output_buffer_size=6 * batch_size)
-
-    if params['flip_horizontally'] and not random.randint(0, 1):
-        data = data.map(_flip_image_horizontally,
-                        num_threads=num_threads, output_buffer_size=6 * batch_size)
-
-    if params['flip_vertically'] and not random.randint(0, 1):
-        data = data.map(_flip_image_vertically, num_threads=num_threads,
-                        output_buffer_size=6 * batch_size)
+        _normalize_data, num_threads=num_threads, output_buffer_size=batch_size)
 
     # Shuffle the data queue
-    data = data.shuffle(len(image_paths))
+    data = data.shuffle(batch_size * 2)
 
     # Create a batch of data
     data = data.batch(batch_size)
-
-    data = data.map(_resize_image, num_threads=num_threads,
-                    output_buffer_size=6 * batch_size)
 
     # Create iterator
     iterator = Iterator.from_structure(data.output_types, data.output_shapes)
